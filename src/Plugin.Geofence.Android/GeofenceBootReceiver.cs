9ff8c499aca0bccb90bf652b201bd6df6cff9f2b
Rendy Del Rosario <rendy.delrosario@gmail.com>
2018-08-06 18:14:05 -0400

    Updated source

---
+++ src/Plugin.Geofence.Android/GeofenceBootReceiver.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Content;
+
+namespace Plugin.Geofence
+{
+    /// <summary>
+    /// GeofenceBootReceiver class
+    /// Receives BOOT_COMPLETED event
+    /// </summary>
+    [BroadcastReceiver]
+    [IntentFilter(new[] { Intent.ActionBootCompleted})]
+    public class GeofenceBootReceiver : BroadcastReceiver
+    {
+        /// <summary>
+        /// On boot completed restores all persisted regions
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="intent"></param>
+        public override void OnReceive(Context context, Intent intent)
+        {
+            System.Diagnostics.Debug.WriteLine(string.Format("{0} - {1}", CrossGeofence.Id, "Boot Received"));
+            var geofenceInstance= CrossGeofence.Current;
+        }
+    }
+}
---
+++ src/Plugin.Geofence.UWP/GeofenceImplementation.cs
@@ -0,0 +1,13 @@
+using Plugin.Geofence.Abstractions;
+using System;
+
+
+namespace Plugin.Geofence
+{
+  /// <summary>
+  /// Implementation for Feature
+  /// </summary>
+  public class GeofenceImplementation : IGeofence
+  {
+  }
+}
